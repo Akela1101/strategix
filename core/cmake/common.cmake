dce92ffdfe99bb1e9692972662f795b741d77054
Akela1101 <akela1101@gmail.com>
2017-11-14 20:36:00 +0300

    update cmake files

--- core/cmake/common.cmake
+++ core/cmake/common.cmake
@@ -1,8 +1,7 @@
 cmake_minimum_required(VERSION 3.9)
 
-set(CMAKE_CXX_STANDARD 17)
 if(MSVC)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /std:c++latest")
+    add_compile_options("/std:c++latest")
 endif(MSVC)
 add_definitions(-DBOOST_MOVE_USE_STANDARD_LIBRARY_MOVE)
 
